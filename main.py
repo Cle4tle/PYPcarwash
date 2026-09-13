d75a8e85cc8de6cf16ace4f7132c6d1994af06a1
cle4tle <mfengze@gmail.com>
2026-09-13 16:29:44 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#main program
+import csv
+adminP="TP091031"
+fieldnames=["names","idnum","pass"]
+def inLogon(process):
+    with open("data/employees.csv", "r") as employees:
+        credentials = csv.DictReader(employees)
+        #print(credentials)
+        if process == "showall":
+            for i in credentials:
+                print(i)
+        elif process == "append":
+            newEmployeeName = input("Enter name of the new employee: ")
+            for ID in credentials:
+                lastId = int(ID["idnum"].strip("SW"))
+            newEmployeeId = f"SW{(4-len(str(lastId+1)))*"0"}{lastId+1}"
+            print("The ID of the new employee is: ",newEmployeeId)
+            newEmployeePass = input("Set a password: ")
+            with open("data/employees.csv","a") as employees:
+                write = csv.DictWriter(employees,fieldnames=fieldnames)
+                write.writerow({"names":newEmployeeName,"idnum":newEmployeeId,"pass":newEmployeePass})
+
+inLogon("showall")
+
+inLogon("append")
+
+inLogon("showall")
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
